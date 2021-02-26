4a0d5c62e01889cbb8c94797b5d02b282347b570
loyalself <plafhtcc@163.com>
2021-02-26 18:08:05 +0800

    5.3. 后台创建和编辑商品

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -11,6 +11,10 @@ Route::group([
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index');
-    $router->get('users', 'UsersController@index');
-    $router->get('products', 'ProductsController@index');
+    $router->get('users', 'UsersController@index');                  //注册用户列表
+    $router->get('products', 'ProductsController@index');            //商品列表
+    $router->get('products/create', 'ProductsController@create');    //创建商品页面
+    $router->post('products', 'ProductsController@store');           //创建商品
+    $router->get('products/{id}/edit', 'ProductsController@edit');   //编辑商品页面
+    $router->put('products/{id}', 'ProductsController@update');      //更新商品
 });
