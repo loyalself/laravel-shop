2562a92000a41ca825e23baf6db79e45799fb7dc
loyalself <plafhtcc@163.com>
2021-03-02 16:12:52 +0800

    7.3-订单支付宝支付

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,8 @@ Route::group(['middleware' => 'auth'], function() {
         Route::post('orders', 'OrdersController@store')->name('orders.store'); //下单
         Route::get('orders', 'OrdersController@index')->name('orders.index'); //订单列表
         Route::get('orders/{order}', 'OrdersController@show')->name('orders.show'); //订单详情
+        Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay'); //支付宝支付
+        Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return'); //前端回调
     });
 });
 
@@ -44,3 +46,5 @@ Route::get('alipay', function() {
         'subject' => 'test subject - 测试',
     ]);
 });
+
+Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');//服务器端回调
